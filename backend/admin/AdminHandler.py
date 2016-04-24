7ae13288157a8af676dc7018d2ee038c61becd05
JLucka <julia.lucka@me.com>
2016-04-24 17:35:26 +0200

    Admin refactor & new options

--- backend/admin/AdminHandler.py
+++ backend/admin/AdminHandler.py
@@ -11,11 +11,7 @@ class AdminHandler(webapp2.RequestHandler):
         current_user = User.query(User.name == users.get_current_user().nickname()).get()
         if users.is_current_user_admin() or current_user.admin:
             current_user.make_admin()
-            self.response.status = 200
-            users_query = User.query().fetch()
-            users_json = []
-            for user in users_query:
-                users_json.append(user.to_json_obj())
+            users_json = User.get_all_as_json()
             self.response.out.write(json.dumps(users_json))
         else:
             self.response.status = 401
@@ -26,24 +22,36 @@ class AdminHandler(webapp2.RequestHandler):
             current_user.make_admin()
             function = self.request.get("function")
             if function == "hide":
-                project = Project.get_by_id(int(str(self.request.get("projectId"))))
-                project.hide()
+                self.hide()
+            elif function == 'make_admin':
+                self.make_admin()
             else:
-                user = User.query(User.google_id == str(self.request.get("userId"))).get()
-                amount = int(self.request.get("amount"))
-                if function == "substract":
-                    user.substract_money(amount)
-                    self.response.out.write(user.to_json_obj())
-                elif function == 'add':
-                    user.add_money(amount)
-                    self.response.out.write(user.to_json_obj())
-                elif function == 'set':
-                    user.set_money(amount)
-                    self.response.out.write(user.to_json_obj())
-                else:
-                    self.response.status = 400
+                self.money_operation(function)
         else:
             self.response.status = 401
+
+    def hide(self):
+        project = Project.get_by_id(int(str(self.request.get("projectId"))))
+        project.hide()
+
+    def make_admin(self):
+        user = User.query(User.google_id == str(self.request.get("userId"))).get()
+        user.make_admin()
+
+    def money_operation(self, function):
+        user = User.query(User.google_id == str(self.request.get("userId"))).get()
+        amount = int(self.request.get("amount"))
+        if function == "substract":
+            user.substract_money(amount)
+            self.response.out.write(user.to_json_obj())
+        elif function == 'add':
+            user.add_money(amount)
+            self.response.out.write(user.to_json_obj())
+        elif function == 'set':
+            user.set_money(amount)
+            self.response.out.write(user.to_json_obj())
+        else:
+            self.response.status = 400
 
 
 app = webapp2.WSGIApplication([('/admin', AdminHandler)])
