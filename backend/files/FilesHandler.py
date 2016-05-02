f606a7769b9d921c7237c6ae09c04fd64d33fedd
JLucka <julia.lucka@me.com>
2016-05-02 13:55:23 +0200

    Break the app

--- backend/files/FilesHandler.py
+++ backend/files/FilesHandler.py
@@ -15,15 +15,15 @@ class UploadLinkHandler(webapp2.RequestHandler):
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         project_id = Project.get_by_id(int(str(self.request.get("projectId")))).key
-        answer = []
-        for i in range(0, len(self.get_uploads())):
-            upload = self.get_uploads()[i]
-            my_file = File()
-            my_file.project = project_id
-            my_file.blobKey = upload.key()
-            my_file.put()
-            answer.append(str(my_file.blobKey))
-        self.response.write(answer)
+        # answer = []
+        # for i in range(0, len(self.get_uploads())):
+        #     upload = self.get_uploads()[i]
+        #     my_file = File()
+        #     my_file.project = project_id
+        #     my_file.blobKey = upload.key()
+        #     my_file.put()
+        #     answer.append(str(my_file.blobKey))
+        self.response.write(self.get_uploads())
 
 
 class DownloadHandler(blobstore_handlers.BlobstoreDownloadHandler):
