6d302ba2b09b05ad0af400d3e49d2f25c6a8266f
magdgar <magdgar@gmail.com>
2016-05-09 12:00:21 +0200

    bugfix

--- backend/admin/AdminHandler.py
+++ backend/admin/AdminHandler.py
@@ -50,7 +50,7 @@ class AdminHandler(webapp2.RequestHandler):
         elif function == 'add_all':
             all_users = User.query().fetch()
             for user in all_users:
-                user.money += 10
+                user.money += amount
                 user.put()
         elif function == 'set':
             user.set_money(amount)
